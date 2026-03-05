dcd7f491704a0d0a2f4d896a8c3f13fdc362bbbf
loutruong <thanhvantruong.vn@gmail.com>
2026-03-05 21:31:30 +0700

    Push

--- social_fullstack_reactjs_expressjs/API/connect.js
+++ social_fullstack_reactjs_expressjs/API/connect.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-    host:"localhost",
+    host: "168.144.41.175",
     user: "root",
-    password:"1234",
-    database:"mydevify_social",
-    port:"3306"
+    password: "1234",
+    database: "mydevify_social",
+    port: "3306"
 }) 
--- social_fullstack_reactjs_expressjs/API/index.js
+++ social_fullstack_reactjs_expressjs/API/index.js
@@ -40,7 +40,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 
-app.post("/api/upload",upload.single("file"), (req,res)=>{
+app.post("/api/upload", upload.single("file"), (req, res) => {
   const file = req.file;
   res.status(200).json(file.filename)
 });
